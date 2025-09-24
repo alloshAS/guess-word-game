b46083f7c04ceedda3ec7eb755530c0080a7df6d
alloshAS <alialhumse820@gmail.com>
2025-09-24 11:50:39 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,15 @@
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("3loush-cache").then(cache => {
+    caches.open("guess-word-game-cache-v1").then(cache => {
       return cache.addAll([
-        "/3loush/",
-        "/3loush/index.html",
-        "/3loush/pro.css",
-        "/3loush/script.js"
+        "/guess-word-game/",
+        "/guess-word-game/index.html",
+        "/guess-word-game/style.css",
+        "/guess-word-game/script.js",
+        "/guess-word-game/images",
+        "/guess-word-game/audio",
+        "/guess-word-game/manifest.json",
+        "/guess-word-game/words.json"
       ]);
     })
   );
